5d52076106130c5804f2c9a8d4edf371ad0c504e
LachezarIvanov-git <lachezar.ivanov.dev@gmail.com>
2023-12-11 04:06:19 +0200

    gallery site added

--- pages/about.js
+++
@@ -1,9 +0,0 @@
-import { render, html } from "../node_modules/lit-html/lit-html.js";
-
-export function about() {
-   
-    const main=document.querySelector('main');
-    const aboutTemplate = () => html`<h2>More about me<h2>`;
-
-    render(aboutTemplate(),main);
-}
