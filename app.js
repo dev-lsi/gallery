7368d8be6f726fad91481c2769659fd2e2f7b6bc
LachezarIvanov-git <lachezar.ivanov.dev@gmail.com>
2023-12-11 17:35:44 +0200

    page and lit-html added to the project

---
+++ app.js
@@ -0,0 +1,33 @@
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+import page from "/node_modules/page/page.mjs";
+
+const main = document.querySelector('main');
+
+function start() {
+
+    page('/index.html', '/');
+    page('/',()=>showHome());
+    page('/galleries',()=>showGalleries());
+    page('/about',()=>showAbout());
+    page('/contacts',()=>showContact());
+    page.start();
+}
+
+function showHome(){
+  const template = ()=> html`<h1>Home Page</h1><p>Welcome: </p><p>Username</p>`;
+  render(template(),main);
+}
+function showGalleries(){
+    const template = ()=> html`<h1>Galleries Page</h1><ul><li>Gal 1</li><li>Gal 2</li><li>Gal 3</li></ul>`;
+    render(template(),main);
+  }
+  function showAbout(){
+    const template = ()=> html`<h1>About Page</h1><p>Personal Info:</p>`; 
+    render(template(),main);
+  }
+  function showContact(){
+    const template = ()=> html`<h1>Contact Page</h1><p>Contacts:</p><p>email:person@ash.com:</p>`;
+    render(template(),main);
+  }
+
+start();
