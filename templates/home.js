e9c56c320d7475becf8ca6b571e6ff00688aa087
LachezarIvanov-git <lachezar.ivanov.dev@gmail.com>
2024-01-01 05:01:30 +0200

    few moments added

--- templates/home.js
+++ templates/home.js
@@ -8,11 +8,12 @@ export async function home(ctx) {
     const template = (ctx) => html`
 
         <div class = home-page-container>
-            <h4>Welcome ${ctx.user.username}</h4>
+            <h3>${ctx.user.username}</h3>
             <div>
+            <h1>Welcome to Zori's Art Gallery!</h1>
             <p>Walk trought the galeries</p>
-            <p> and find a lot of fine arts on canvas and paper</p>
-            <p>A lot of oil and watercolor paintings</p>
+            <p> and find a lot of fine arts on canvas and paper.</p>
+            <p>A lot of oil and watercolor paintings.</p>
             <p>Also you will see my charcoal art and a lot of other drawings!</p>
             </div>
 
@@ -24,6 +25,7 @@ export async function home(ctx) {
                 <div class = "image-div"><img src = "../assets/charcoal03.jpg" alt="charcoal03.jpg"/></div>
                 <div class = "image-div"><img src = "../assets/charcoal04.jpg" alt="charcoal04.jpg"/></div>
                 <div class = "image-div"><img src = "../assets/charcoal05.jpg" alt="charcoal05.jpg"/></div>
+               
                 <div class = "image-div"><img src = "../assets/paint-tools01.png" alt="tools.png"/></div>
             </section>
 
@@ -32,30 +34,26 @@ export async function home(ctx) {
             
 
             <div class = "link-section">
+                <a href="/galleries/oil">Oil and Water color</a>
                 <div><img src = "../assets/sunflowers.jpg" alt="sunflowers.jpg"/></div>
                 <div><img src = "../assets/waterfall.jpg" alt="waterfall.jpg"/></div>
                 <div><img src = "../assets/coastal.jpg" alt="coastal.jpg"/></div>
-                <a href="/galleries/oil">Oil and Water color</a>
             </div>
 
             <div class = "link-section">
-                <div><img src = "../assets/charcoal01.jpg" alt="sunflowers.jpg"/></div>
-                <div><img src = "../assets/charcoal02.jpg" alt="charcoal01.jpg"/></div>
-                <div><img src = "../assets/charcoal03.jpg" alt="drawingso1.jpg"/></div>
-                <a href="/galleries/charcoal">Charcoal</a>
+                <a href="/galleries/oil">Charcoal</a>
+                <div><img src = "../assets/cc01.jpg" alt="cc.jpg"/></div>
+                <div><img src = "../assets/cc02.jpg" alt="cc.jpg"/></div>
+                <div><img src = "../assets/cc03.jpg" alt="cc.jpg"/></div>
             </div>
 
             <div class = "link-section">
+                <a href="/galleries/scratches">Scratches</a>
                 <div><img src = "../assets/yellow.jpg" alt="sunflowers.jpg"/></div>
                 <div><img src = "../assets/images.jpg" alt="charcoal01.jpg"/></div>
                 <div><img src = "../assets/drawings01.jpg" alt="drawingso1.jpg"/></div>
-                <a href="/galleries/scratches">Scratches</a>
-                
             </div>
 
-            <a href="/galleries/oil">Oil & Water-color Paintings</a>
-            <a href="/galleries">Charcoal</a>
-            <a href="/galleries">Drawings</a>
 
         </div>
 
